037b413c413944d3997a8e22e4ead8a44b816c45
Deyan Ginev <deyan.ginev@telerik.com>
2016-02-16 11:48:33 +0200

    Removed unused javascript.

--- fresco/fresco-common.ts
+++ fresco/fresco-common.ts
@@ -29,29 +29,3 @@ export class FrescoDrawee extends viewModule.View {
         
     }
 }
-
-var FrescoDrawee = (function (_super) {
-    __extends(FrescoDrawee, _super);
-    function FrescoDrawee() {
-        _super.apply(this, arguments);
-    }
-    FrescoDrawee.onImageUriPropertyChanged = function (args) {
-        var drawee = args.object;
-        drawee.onImageUriChanged(args);
-    };
-    FrescoDrawee.prototype.onImageUriChanged = function (args) {
-    };
-    Object.defineProperty(FrescoDrawee.prototype, "imageUri", {
-        get: function () {
-            return this._getValue(FrescoDrawee.imageUriProperty);
-        },
-        set: function (uri) {
-            this._setValue(FrescoDrawee.imageUriProperty, uri);
-        },
-        enumerable: true,
-        configurable: true
-    });
-    FrescoDrawee.imageUriProperty = new dependencyObservable.Property("imageUri", "FrescoDrawee", new proxyModule.PropertyMetadata(undefined, dependencyObservable.PropertyMetadataSettings.None, FrescoDrawee.onImageUriPropertyChanged));
-    return FrescoDrawee;
-})(view_1.View);
-exports.FrescoDrawee = FrescoDrawee;
