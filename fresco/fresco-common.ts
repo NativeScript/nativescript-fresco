ea9018427e72c7b570baa62d643d4402f33cb610
Deyan Ginev <deyan.ginev@telerik.com>
2016-02-16 11:48:13 +0200

    Removed unused extend.

--- fresco/fresco-common.ts
+++ fresco/fresco-common.ts
@@ -1,8 +1,3 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
 import viewModule = require("ui/core/view");
 import dependencyObservable = require("ui/core/dependency-observable");
 import proxyModule = require("ui/core/proxy");
