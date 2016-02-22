05106d82e42ca7f49e86df414218164bb98e899d
Deyan Ginev <ginev@ginevmac.bedford.progress.com>
2016-02-22 18:56:52 +0200

    Fixed a bug with dep properties

--- fresco/fresco-common.ts
+++ fresco/fresco-common.ts
@@ -3,29 +3,29 @@ import dependencyObservable = require("ui/core/dependency-observable");
 import proxyModule = require("ui/core/proxy");
 
 export class FrescoDrawee extends viewModule.View {
-    
+
     private static imageUriProperty = new dependencyObservable.Property(
         "imageUri",
         "FrescoDrawee",
         new proxyModule.PropertyMetadata(
-            undefined, 
+            undefined,
             dependencyObservable.PropertyMetadataSettings.None,
             FrescoDrawee.onImageUriPropertyChanged));
-    
+
     get imageUri(): string{
-        return this.getValue(FrescoDrawee.imageUriProperty);
+        return this._getValue(FrescoDrawee.imageUriProperty);
     }
-    
+
     set imageUri(value: string){
-        this.setValue(FrescoDrawee.imageUriProperty, value);
+        this._setValue(FrescoDrawee.imageUriProperty, value);
     }
-    
+
     private static onImageUriPropertyChanged(args){
                var drawee = args.object;
         drawee.onImageUriChanged(args);
     }
-    
+
     protected onImageUriChanged(args){
-        
+
     }
 }
