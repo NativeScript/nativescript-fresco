1b0400af8c53683f57b8bb5a727ac47fff69408e
VladimirAmiorkov <v.amiorkov@gmail.com>
2018-07-17 17:51:43 +0300

    chore: Add "exit" event handler and call shut down for the nativescript-fresco plugin

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,22 +1,18 @@
-/*
-In NativeScript, the app.ts file is the entry point to your application.
-You can use this file to perform app-level initialization, but the primary
-purpose of the file is to pass control to the app’s first module.
-*/
-
 import * as app from "application";
 
 import * as frescoModule from "nativescript-fresco";
 
 if (app.android) {
-    app.on("launch", () => {
+    app.on(app.launchEvent, () => {
         frescoModule.initialize({ isDownsampleEnabled: true });
+    });
+
+    app.on(app.exitEvent, (args) => {
+        if (args.android) {
+            console.log("dev-log: Manually shutting down Fresco");
+            frescoModule.shutDown();
+        }
     });
 }
 
 app.run({ moduleName: "app-root" });
-
-/*
-Do not place any code after the application has been started as it will not
-be executed on iOS.
-*/
